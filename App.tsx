d78068542b4c50a5294e46e3144668d63db0f648
Sanjay17u <sanjay.solanki.17.om@gmail.com>
2025-12-11 18:16:13 +0530

    1st - Way of Complete.

--- App.tsx
+++ App.tsx
@@ -1,45 +1,80 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- */
+import React, { useState } from 'react'
+import { View, Text, StyleSheet, TextInput, Button } from 'react-native'
 
-import { NewAppScreen } from '@react-native/new-app-screen';
-import { StatusBar, StyleSheet, useColorScheme, View } from 'react-native';
-import {
-  SafeAreaProvider,
-  useSafeAreaInsets,
-} from 'react-native-safe-area-context';
+
 
 function App() {
-  const isDarkMode = useColorScheme() === 'dark';
+
+  const [isHave, setIsHave] = useState(true)
+  const [count, setCount] = useState(0)
+  const [inputValue, setInputValue] = useState("");
+
+
+  const countHandler = (text: any) => {
+    if (text.length > inputValue.length) {
+
+      setCount(() => count + 1)
+
+    } else if (text.length < inputValue.length) {
+
+      setCount(() => count - 1)
+
+    }
+    
+    if (text.length === 0 || text.length >= 20) {
+      setIsHave(false)
+    } else {
+      setIsHave(true)
+    }
+
+    setInputValue(text)
+
+  }
+
+  const clearCount = () => {
+    setCount(0)
+    setInputValue('')
+    setIsHave(true)
+  }
 
   return (
-    <SafeAreaProvider>
-      <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
-      <AppContent />
-    </SafeAreaProvider>
-  );
-}
-
-function AppContent() {
-  const safeAreaInsets = useSafeAreaInsets();
-
-  return (
+    <>
     <View style={styles.container}>
-      <NewAppScreen
-        templateFileName="App.tsx"
-        safeAreaInsets={safeAreaInsets}
-      />
+      <Text style={styles.countText}> { count } </Text>    
+        <TextInput editable={ isHave } value={inputValue} onChangeText={ (text) => countHandler(text) } style={styles.InputCounter} />  
+        <Button onPress={ clearCount } disabled={ isHave } title='Clear Text'/>  
     </View>
-  );
+    </>
+  )
 }
+
+
+
+export default App
+
 
 const styles = StyleSheet.create({
   container: {
+    backgroundColor: 'black',
+    display: 'flex',
     flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center'
   },
-});
 
-export default App;
+  countText: {
+    color: 'white',
+    fontSize: 30,
+    fontWeight: '700',
+
+  },
+
+  InputCounter: {
+    borderWidth: 1,
+    borderColor: 'gray',
+    width: '80%',
+    borderRadius: 5,
+    marginVertical: 30,
+    color: 'white'
+  }
+})
